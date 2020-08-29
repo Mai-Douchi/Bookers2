f890bfc05a22b564202e80eb262d66ae0d1ea98c
vagrant <vagrant@localhost.localdomain>
2020-08-29 07:38:32 +0000

    [Finish]Bookers2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2b915e284b34a47b12749e58dd6a6b28fdab9b24d4342e4f95988c442d852df5e11fd5fdf6c75f070f333429e6f1f2b93829f7bb6f429c2cefdb58c54f26d216'
